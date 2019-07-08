5b41538a3e7fc8b9cf9417cd698d075b63c3fc7e
Allen Gong <hollomyfoolish@163.com>
2019-07-08 10:36:44 +0800

    update go-repo

--- repo.go
+++ repo.go
@@ -1,5 +1,9 @@
 package repo
 
+import(
+	"github.com/hollomyfoolish/go-repo/p2"
+)
+
 func Repo() string {
-	return "github.com/hollomyfoolish/go-repo"
+	return "github.com/hollomyfoolish/go-repo: " + p2.Echo()
 }
